f8d426ff3b08ce9a5fce71a1eb3a356a26ec862e
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-10 14:47:26 +0000

    Add suppliers views

---
+++ api/v1/views/suppliers.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""View for Supplier objects that handle all default RESTFul API actions"""
+from flask import jsonify, abort, request
+
+from api.v1.views import app_views
+from models import storage
+from models.supplier import Supplier
+
+
+@app_views.route("/suppliers", methods=['GET'])
+def get_suppliers():
+    """Retrieves list of all Supplier objects"""
+    all_suppliers = storage.all(Supplier).values()
+    return jsonify([supplier.to_dict for supplier in all_suppliers])
+
+
+@app_views.route("/suppliers/<supplier_id>", methods=['GET'])
+def get_supplier(supplier_id):
+    """Retrieves a specific Supplier"""
+    supplier = storage.get(Supplier, supplier_id)
+    if not supplier:
+        abort(404)
+    return jsonify(supplier.to_dict)
+
+
+@app_views.route("/suppliers", methods=['POST'])
+def post_supplier():
+    """Creates a new Supplier"""
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    if "name" not in data:
+        abort(400, description="Missing name")
+    if "phone" not in data:
+        abort(400, description="Missing phone")
+    new_supplier = Supplier(**data)
+    new_supplier.save()
+    return jsonify(instance.to_dict()), 201
+
+
+@app_views.route("/suppliers/<supplier_id>", methods=['PUT'])
+def put_supplier(supplier_id):
+    """Updates a Supplier"""
+    supplier = storage.get(Supplier, supplier_id)
+    if not supplier:
+        abort(404)
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    ignore_keys = ["id", "created_at", "updated_at"]
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(supplier, key, value)
+    supplier.save()
+    return jsonify(supplier.to_dict()), 200
+
+
+# Implementation of DELETE method might be disabled
+
+@app_views.route("/suppliers/<supplier_id>", methods=['DELETE'])
+def delete_supplier(supplier_id):
+    """Deletes a Supplier object"""
+    supplier = storage.get(Supplier, supplier_id)
+    if not supplier:
+        abort(400)
+    storage.delete(supplier)
+    storage.save()
+    return jsonify({}), 200
