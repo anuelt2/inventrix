c51375bd1b5dde46dc25d5115b2f8c3dd4f4edde
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-10 10:39:27 +0000

    Implement strict_slashes globally in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from api.v1.views import app_views
 from api.v1.auth import app_auth
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
 app.register_blueprint(app_auth)
