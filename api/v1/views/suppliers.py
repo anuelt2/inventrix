b745cd1fb3036bc1ea2abd0b5274dffab235c30f
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-11 12:36:35 +0000

    Implement checks for existing unique values in post and put requests in Supplier views

--- api/v1/views/suppliers.py
+++ api/v1/views/suppliers.py
@@ -33,6 +33,21 @@ def post_supplier():
         abort(400, description="Missing name")
     if "phone" not in data:
         abort(400, description="Missing phone")
+    if "email" not in data:
+        abort(400, description="Missing email")
+
+    existing_supplier = storage.get_by_attr(Supplier, "name", data["name"])
+    if existing_supplier:
+        abort(400, description="Supplier name taken")
+
+    existing_supplier = storage.get_by_attr(Supplier, "phone", data["phone"])
+    if existing_supplier:
+        abort(400, description="Supplier phone number taken")
+
+    existing_supplier = storage.get_by_attr(Supplier, "email", data["email"])
+    if existing_supplier:
+        abort(400, description="Supplier email taken")
+
     new_supplier = Supplier(**data)
     new_supplier.save()
     return jsonify(new_supplier.to_dict()), 201
@@ -46,8 +61,25 @@ def put_supplier(supplier_id):
         abort(404)
     if not request.is_json:
         abort(400, description="Not a JSON")
+
     data = request.get_json()
     ignore_keys = ["id", "created_at", "updated_at"]
+
+    if "name" in data and data["name"] != supplier.name:
+        existing_supplier = storage.get_by_attr(Supplier, "name", data["name"])
+        if existing_supplier and existing_supplier[0].id != supplier.id:
+            abort(400, description="Supplier name taken")
+
+    if "phone" in data and data["phone"] != supplier.name:
+        existing_supplier = storage.get_by_attr(Supplier, "phone", data["phone"])
+        if existing_supplier and existing_supplier[0].id != supplier.id:
+            abort(400, description="Supplier phone number taken")
+
+    if "email" in data and data["email"] != supplier.name:
+        existing_supplier = storage.get_by_attr(Supplier, "email", data["email"])
+        if existing_supplier and existing_supplier[0].id != supplier.id:
+            abort(400, description="Supplier email taken")
+
     for key, value in data.items():
         if key not in ignore_keys:
             setattr(supplier, key, value)
