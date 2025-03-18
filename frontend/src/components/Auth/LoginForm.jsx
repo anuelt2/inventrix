9029ad7aaec43809621c7968fa2facf0d8dd2c74
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-18 15:19:08 +0000

    Add AuthContext

--- frontend/src/components/Auth/LoginForm.jsx
+++ frontend/src/components/Auth/LoginForm.jsx
@@ -1,47 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Form from "../UI/InputForm";
-import API from "../../utils/api";
-
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
 
   const fields = [
     {
       name: "email",
-      label: "Email",
       type: "email",
-      placeholder: "Enter your email"
+      placeholder: "Email",
     },
     {
       name: "password",
-      label: "Password",
       type: "password",
-      placeholder: "Enter your password"
+      placeholder: "Password",
     },
   ];
 
   const handleSubmit = async (formData) => {
     console.log("Submitting:", formData);
     try {
-      const response = await API.post("/auth/login", formData);
-      localStorage.setItem("access_token", response.tokens.access);
+      await login(formData);
       navigate("/dashboard");
     } catch (error) {
-      setErrors({ general: "Invalid email or password" });
+      setErrors({ general: error || "Invalid email or password" });
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="text-gray-600 w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
       <Form
         fields={fields}
         onSubmit={handleSubmit}
         buttonText="Login"
-        title="User Login"
+        title="Login"
         errorMessages={errors}
       />
     </div>
