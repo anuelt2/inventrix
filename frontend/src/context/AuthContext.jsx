e98f366344f2693b161f1f7ac0eb25081f92bb6d
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-18 15:20:25 +0000

    Add AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,8 +1,56 @@
 import React, { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import { loginUser, fetchUserData, logoutUser } from "../services/AuthService";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    
-}
+  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("accessToken") || null
+  );
+  const [error, setError] = useState("");
+
+  // Fetch user details with access token
+  useEffect(() => {
+    const getUser = async () => {
+      if (accessToken) {
+        try {
+          const userData = await fetchUserData();
+          setUser(userData);
+        } catch (error) {
+          setError(error);
+          logout();
+        }
+      }
+    };
+
+    getUser();
+  }, [accessToken]);
+
+  // Login function
+  const login = async (credentials) => {
+    try {
+      const { accessToken, refreshToken } = await loginUser(credentials);
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      setAccessToken(accessToken);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  // Logout function
+  const logout = () => {
+    logoutUser();
+    setUser(null);
+    setAccessToken(null);
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, accessToken, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
