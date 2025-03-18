d4a1ba9dba5351a887663447ce14e664e60e72fb
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-18 08:54:39 +0000

    Add AuthProvider

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,8 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    
+}
