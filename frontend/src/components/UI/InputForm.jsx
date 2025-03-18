9305915710b0f882f12a930d9c1c323c57471a04
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-18 15:20:03 +0000

    Apply styling

--- frontend/src/components/UI/InputForm.jsx
+++ frontend/src/components/UI/InputForm.jsx
@@ -24,60 +24,62 @@ export const Form = ({
   };
 
   return (
-    <div className="w-full max-w-lg p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">{title}</h2>
-      <form onSubmit={handleSubmit} className="space-y-4 border-gray-600">
-        {fields.map((field) => (
-          <div key={field.name}>
-            <label className="block text-gray-700 font-semibold mb-1">
-              {field.label}
-            </label>
-            {field.type === "select" ? (
-              <select
-                name={field.name}
-                value={formData[field.name] || ""}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-300 rounded-lg shadow-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300"
-              >
-                {field.options.map((option) => (
-                  <option key={option.id} id={option.id} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            ) : field.type === "textarea" ? (
-              <textarea
-                name={field.name}
-                value={formData[field.name] || ""}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-300 rounded-lg shadow-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300"
-                rows="4"
-              />
-            ) : (
-              <input
-                type={field.type}
-                name={field.name}
-                value={formData[field.name] || ""}
-                onChange={handleChange}
-                placeholder={field.placeholder}
-                autoComplete=""
-                className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300"
-              />
-            )}
-            {errorMessages[field.name] && (
-              <p className="text-red-500 text-sm mt-1">
-                {errorMessages[field.name]}
-              </p>
-            )}
-          </div>
-        ))}
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
-        >
-          {buttonText}
-        </button>
-      </form>
+    <div className="fixed inset-0 flex items-center justify-center min-h-screen w-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+        <h2 className="text-gray-900 text-2xl font-bl=old mb-4 text-center">{title}</h2>
+        <form onSubmit={handleSubmit} className="space-y-4 border-gray-600">
+          {fields.map((field) => (
+            <div key={field.name}>
+              <label className="block text-gray-700 font-semibold mb-1">
+                {field.label}
+              </label>
+              {field.type === "select" ? (
+                <select
+                  name={field.name}
+                  value={formData[field.name] || ""}
+                  onChange={handleChange}
+                  className="w-full p-3 border border-gray-300 rounded shadow-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300"
+                >
+                  {field.options.map((option) => (
+                    <option key={option.id} id={option.id} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              ) : field.type === "textarea" ? (
+                <textarea
+                  name={field.name}
+                  value={formData[field.name] || ""}
+                  onChange={handleChange}
+                  className="w-full p-3 border border-gray-300 rounded shadow-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300"
+                  rows="4"
+                />
+              ) : (
+                <input
+                  type={field.type}
+                  name={field.name}
+                  value={formData[field.name] || ""}
+                  onChange={handleChange}
+                  placeholder={field.placeholder}
+                  autoComplete=""
+                  className="w-full p-3 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300"
+                />
+              )}
+              {errorMessages[field.name] && (
+                <p className="text-red-500 text-sm mt-2">
+                  {errorMessages[field.name]}
+                </p>
+              )}
+            </div>
+          ))}
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300"
+          >
+            {buttonText}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
