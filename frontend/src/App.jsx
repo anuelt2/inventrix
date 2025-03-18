7b054f97e9c9151fbd89f6d2a8233b0aa73986b6
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-18 08:10:26 +0000

    Add route handling to react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import Dashboard from './components/Dashboard/DashboardLayout'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from './components/Dashboard/DashboardLayout';
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 import './App.css'
 
 
 const App = () => {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 };
 
