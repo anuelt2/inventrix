0d935c7b52727e262c4716b9e090065c2c938b27
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-03-19 18:09:48 +0000

    Render correct total for suppliers and products on dashboard

--- frontend/src/components/Dashboard/DashboardLayout.jsx
+++ frontend/src/components/Dashboard/DashboardLayout.jsx
@@ -1,18 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useAuth } from "../../context/AuthContext";
 import OverviewSection from "./OverviewSection";
+import axios from "axios";
 
 const Dashboard = ({ children }) => {
   const { accessToken } = useAuth();
   const navigate = useNavigate();
 
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [totalSuppliers, setTotalSuppliers] = useState(0);
+  
+  const getTotalProducts = async () => {
+    await axios.get('http://localhost:5000/api/v1/products')
+    .then(response => setTotalProducts(response.data.total))
+    .catch(error => console.error(error))
+  }
+
+  const getTotalSuppliers = async () => {
+    await axios.get('http://localhost:5000/api/v1/suppliers')
+    .then(response => setTotalSuppliers(response.data.total))
+    .catch(error => console.error(error))
+  }
+
   useEffect(() => {
     if (!accessToken) {
       navigate("/login");
     }
-  }, [accessToken, navigate]);
+
+    getTotalProducts();
+    getTotalSuppliers();
+  }, [accessToken, navigate, setTotalProducts, setTotalSuppliers]);
 
   if (!accessToken) {
     return <p>Redirecting...</p>;
@@ -23,9 +42,9 @@ const Dashboard = ({ children }) => {
       <div className="flex flex-1 w-full">
         <OverviewSection
           data={{
-            totalProducts: 100,
+            totalProducts: totalProducts,
             totalSales: 5000,
-            totalSuppliers: 20,
+            totalSuppliers: totalSuppliers,
           }}
         />
       </div>
