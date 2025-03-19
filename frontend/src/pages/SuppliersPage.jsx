b0a548e9eedbe17781d0309eac0068344e456ae0
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-19 09:20:14 +0000

    Add SuppliersPage

---
+++ frontend/src/pages/SuppliersPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SuppliersTable from "../components/Tables/SuppliersTable";
+
+const SuppliersPage = () => {
+  return (
+    <div>
+      <h1>Suppliers</h1>
+      <SuppliersTable />
+    </div>
+  );
+};
+
+export default SuppliersPage;
