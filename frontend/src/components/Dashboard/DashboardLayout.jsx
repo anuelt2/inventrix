d21b173a3b84d0816a48df682ca1baf7858c0bd3
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-19 01:14:40 +0000

    Add suppliers table and style

--- frontend/src/components/Dashboard/DashboardLayout.jsx
+++ frontend/src/components/Dashboard/DashboardLayout.jsx
@@ -1,22 +1,25 @@
-import Layout from '../Layout/Layout';
-import DataTableComponent from '../UI/DataTable';
-import OverviewSection from './OverviewSection';
-
+import Layout from "../Layout/Layout";
+import SuppliersTable from "../Tables/SuppliersTable";
+import OverviewSection from "./OverviewSection";
 
 const Dashboard = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Layout>
         <div className="flex flex-1 w-full">
-          <OverviewSection data={{
-            totalProducts: 100,
-            totalSales: 5000,
-            totalSuppliers: 20
-          }} />
-      </div>
-      
-      {/* Data Table */}
-      <div><DataTableComponent /></div>
+          <OverviewSection
+            data={{
+              totalProducts: 100,
+              totalSales: 5000,
+              totalSuppliers: 20,
+            }}
+          />
+        </div>
+
+        {/* Data Table */}
+        <div className="bg-white shadow-lg rounded-lg p-4 mt-6 overflow-x-auto">
+          <SuppliersTable />
+        </div>
       </Layout>
     </div>
   );
