1a0aaeb393b6fc2a069d63952cdbdf666f4d0390
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-03-23 11:56:16 +0000

    Implement pagination in pages

--- frontend/src/components/Tables/UsersTable.jsx
+++ frontend/src/components/Tables/UsersTable.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import DataTableComponent from "../UI/DataTable";
 import useFetchData from "../UI/FetchTableData";
+import useCreateColumns from "../UI/TableColumns";
 
 const UsersTable = () => {
-  const { data, columns, error } = useFetchData("/users");
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const { data, totalRows, error } = useFetchData("/users", page, limit);
+  const columns = useCreateColumns(data);
 
   if (error) return <p>{error}</p>;
 
-  return <DataTableComponent title="Users" columns={columns} data={data} />;
+  // Update state when user changes pages
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  // Update state when user changes rows per page
+  const handleRowsPerPageChange = (newLimit) => {
+    setLimit(newLimit);
+    setPage(1);
+  };
+
+  return (
+    <DataTableComponent
+      title="Users"
+      columns={columns}
+      data={data}
+      totalRows={totalRows}
+      onPageChange={handlePageChange}
+      onRowsPerPageChange={handleRowsPerPageChange}
+      paginationInfo={{ page, limit }}
+    />
+  );
 };
 
 export default UsersTable;
